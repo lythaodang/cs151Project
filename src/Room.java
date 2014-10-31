703c6e52f3aa791f6a9f41ae24477a0a4e1d99c3
mikephe <mikephee00@gmail.com>
2014-10-30 23:27:35 -0700

    Changed to interface

--- src/Room.java
+++ src/Room.java
@@ -1,13 +1,4 @@
-public class Room
+public interface Room
 {
-	private int roomNum;
-	private boolean isReserved;
-	private Account reservedBy;
 	
-	public Room(int roomNum)
-	{
-		this.roomNum = roomNum;
-		isReserved = false;
-		reservedBy = null;
-	}
 }
