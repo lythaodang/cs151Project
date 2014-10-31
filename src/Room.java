8fc342f75c926d28a1870d4929b664e0fa240743
lythaodang <dang.ly.thao@gmail.com>
2014-10-30 23:10:05 -0700

    why is it giving me an error?

--- src/Room.java
+++ src/Room.java
@@ -1,4 +1,13 @@
-
-public class Room {
-
+public class Room
+{
+	private int roomNum;
+	private boolean isReserved;
+	private Account reservedBy;
+	
+	public Room(int roomNum)
+	{
+		this.roomNum = roomNum;
+		isReserved = false;
+		reservedBy = null;
+	}
 }
