d45e822a9f4dfe5443828f1fe34fcde91b2f0917
mikephe <mikephee00@gmail.com>
2014-10-30 23:48:05 -0700

    Added requirements for Room interface

--- src/Room.java
+++ src/Room.java
@@ -1,4 +1,29 @@
+/**
+   COPYRIGHT (C) 2014 Infinite Loops. All Rights Reserved.
+   A room for reservation.
+   Solves CS151 Project
+   @author Mike Phe, Ly Dang, Andrew Yobs
+   @version 1.00 2014/11/03
+*/
+
 public interface Room
 {
+	/**
+	 	Gets the cost
+	 	@return the cost
+	 */
+	double getCost();
 	
+	/**
+	 	Checks if room is reserved
+	 	@return boolean if room is reserved or not
+	 */
+	boolean isReserved();
+	
+	/**
+	 	Gets who is reserving the room
+	 	@param account the account
+	 	@return the account
+	 */
+	Account reservedBy(Account account);
 }
